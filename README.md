aa1633c8dc649a790e7fb558dc3c72d13abdbb1a
ERodriguez-10 <erodriguez.rrii@gmail.com>
2023-11-03 18:31:50 -0300

    Segundo desafío. Se trabajó con fs.promises

--- README.md
+++ README.md
@@ -10,4 +10,22 @@ Cada nuevo desafío se encuentra en una rama diferente, y se puede acceder a los
 ### Contenido
 
 - [Desafío 1] - Clases ECMAScript y ECMAScript avanzado. Fecha de entrega: 26/10/2023
-- [Desafío 2] - Manejo de archivos. Fecha de entrega: 02/11/2023
+- [Desafío 2] - Manejo de archivos. Fecha de entrega: 02/11/2023.
+
+Al llamar al constructor de ProductManager se guarda en memoria la información del archivo si es que este existe. Esto se hizo para no realizar multiples lecturas al mismo y afectar la performance. En caso de no existir el archivo, se inicializa vacio. Además se utilizó fs.promises.
+
+El resultado final de 'products.json' según los tests existentes debería ser:
+
+`[
+	{
+		"title": "producto prueba 2",
+		"description": "Este es un producto prueba para la generacion de ID",
+		"price": 600,
+		"thumbnail": "Sin imagen",
+		"code": "16712HD",
+		"stock": 25,
+		"id": 2
+	}
+]`
+
+- [Desafío 3] - Próximamente.
--- desafio-2.js
+++ desafio-2.js
@@ -2,40 +2,40 @@ const fs = require("fs");
 
 class ProductManager {
   constructor(pathFileName) {
-    this.products = [];
-    this.currentId = 0;
     this.path = pathFileName;
+    try {
+      let productsFile = fs.readFileSync(this.path, "utf-8");
+      this.products = JSON.parse(productsFile);
+    } catch {
+      this.products = [];
+    }
   }
 
-  setIdProduct() {
-    let idProduct = this.currentId + 1;
-    this.currentId = idProduct;
+  async saveFile() {
+    try {
+      await fs.promises.writeFile(
+        this.path,
+        JSON.stringify(this.products, null, "\t"),
+        "utf-8"
+      );
+    } catch (error) {
+      console.log(`[ERROR] ${error}`);
+    }
   }
 
   getProducts() {
-    if (fs.existsSync(this.path)) {
-      const data = fs.readFileSync(this.path, "utf-8");
-      console.log(JSON.parse(data));
-    } else {
-      console.log(this.products);
-    }
+    console.log(this.products);
   }
 
   getProductById(id) {
-    if (fs.existsSync(this.path)) {
-      const data = fs.readFileSync(this.path, "utf-8");
+    const productSelected = this.products.find((p) => p.id == id);
 
-      const productSelected = JSON.parse(data).find((p) => p.id == id);
-
-      productSelected
-        ? console.log(productSelected)
-        : console.log("[ERROR] Product not found");
-    } else {
-      console.log("[ERROR] We don't have any product");
-    }
+    productSelected
+      ? console.log(productSelected)
+      : console.log("[ERROR] Product not found");
   }
 
-  addProduct(product) {
+  async addProduct(product) {
     let error = false;
     Object.values(product).forEach((value) => {
       if (value == undefined || value == null) {
@@ -50,59 +50,53 @@ class ProductManager {
       if (exist) {
         console.log("[ERROR] Product code already exist!");
       } else {
-        this.setIdProduct();
-        this.products.push({ ...product, id: this.currentId });
+        const newProduct = { ...product, id: this.products.length + 1 };
+        this.products.push(newProduct);
 
-        fs.writeFileSync(this.path, JSON.stringify(this.products), "utf-8");
+        await this.saveFile();
+
+        console.log("Product added successfully.");
       }
     }
   }
 
-  updateProduct(id, object) {
-    if (fs.existsSync(this.path)) {
-      const data = fs.readFileSync(this.path, "utf-8");
+  async updateProduct(id, object) {
+    let foundIndex = undefined;
+    const productSelected = this.products.find((p, index) => {
+      foundIndex = index;
+      return p.id == id;
+    });
 
-      let foundIndex = undefined;
-      const productSelected = JSON.parse(data).find((p, index) => {
-        foundIndex = index;
-        return p.id == id;
-      });
-
-      if (productSelected) {
-        const keyToModify = Object.keys(object);
-        const valuesToSet = Object.values(object);
-        for (let i = 0; i < keyToModify.length; i++) {
-          productSelected[keyToModify[i]] = valuesToSet[i];
-        }
-
-        this.products[foundIndex] = productSelected;
-
-        fs.writeFileSync(this.path, JSON.stringify(this.products), "utf-8");
-      } else {
-        console.log("[ERROR] Product not found");
+    if (productSelected) {
+      const keyToModify = Object.keys(object);
+      const valuesToSet = Object.values(object);
+      for (let i = 0; i < keyToModify.length; i++) {
+        productSelected[keyToModify[i]] = valuesToSet[i];
       }
+
+      this.products[foundIndex] = productSelected;
+
+      await this.saveFile();
+
+      console.log("Product updated successfully.");
     } else {
-      console.log("[ERROR] We don't have any product");
+      console.log("[ERROR] Product not found");
     }
   }
 
-  deleteProduct(id) {
-    if (fs.existsSync(this.path)) {
-      const data = fs.readFileSync(this.path, "utf-8");
+  async deleteProduct(id) {
+    const productSelected = this.products.find((p) => p.id == id);
 
-      const productSelected = JSON.parse(data).find((p) => p.id == id);
+    if (productSelected) {
+      const newArrayProducts = this.products.filter((p) => p.id != id);
 
-      if (productSelected) {
-        const data = fs.readFileSync(this.path, "utf-8");
+      this.products = newArrayProducts;
 
-        const newArrayProducts = JSON.parse(data).filter((p) => p.id != id);
+      await this.saveFile();
 
-        this.products = newArrayProducts;
-
-        fs.writeFileSync(this.path, JSON.stringify(this.products), "utf-8");
-      } else {
-        console.log("[ERROR] ID Product not found. 0 delete product.");
-      }
+      console.log("Product deleted successfully.");
+    } else {
+      console.log("[ERROR] ID Product not found. 0 delete product.");
     }
   }
 }
@@ -120,102 +114,109 @@ class Product {
 
 //TESTING
 
-//Se creará una instancia de la clase "ProductManager"
-const ProductManagerTest = new ProductManager("./products.json");
+async function runTests() {
+  //Se creará una instancia de la clase "ProductManager"
+  const ProductManagerTest = new ProductManager("./products.json");
 
-//Se llamará "getProducts" recién creada la instancia, debe devolver un arreglo vacío
-console.log("[TEST #1] getProducts: Array vacío al instanciarse la clase.");
+  //Se llamará "getProducts" recién creada la instancia, debe devolver un arreglo vacío
+  console.log("[TEST #1] getProducts: Array vacío al instanciarse la clase.");
 
-ProductManagerTest.getProducts();
+  ProductManagerTest.getProducts();
 
-console.log(" ");
+  console.log(" ");
 
-//Se llamará al método "addProduct" con los campos:
-/*
-title: "producto prueba"
-description: "Este es un producto prueba"
-price: 200,
-thumbnail: "Sin imagen",
-code: "abc123",
-stock: 25
-*/
-console.log(
-  "[TEST #2] addProduct: Crear un productos con los campos del test."
-);
+  //Se llamará al método "addProduct" con los campos:
+  /*
+  title: "producto prueba"
+  description: "Este es un producto prueba"
+  price: 200,
+  thumbnail: "Sin imagen",
+  code: "abc123",
+  stock: 25
+  */
+  console.log(
+    "[TEST #2] addProduct: Crear un productos con los campos del test."
+  );
 
-ProductManagerTest.addProduct(
-  new Product(
-    "producto prueba",
-    "Este es un producto prueba",
-    200,
-    "Sin imagen",
-    "abc123",
-    25
-  )
-);
+  await ProductManagerTest.addProduct(
+    new Product(
+      "producto prueba",
+      "Este es un producto prueba",
+      200,
+      "Sin imagen",
+      "abc123",
+      25
+    )
+  );
 
-//El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE
-ProductManagerTest.addProduct(
-  new Product(
-    "producto prueba 2",
-    "Este es un producto prueba para la generacion de ID",
-    600,
-    "Sin imagen",
-    "16712HD",
-    25
-  )
-);
+  //El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE
+  await ProductManagerTest.addProduct(
+    new Product(
+      "producto prueba 2",
+      "Este es un producto prueba para la generacion de ID",
+      600,
+      "Sin imagen",
+      "16712HD",
+      25
+    )
+  );
 
-//Se llamará el método "getProducts" nuevamente, esta vez debe aparecer el producto recién agregado
-console.log(
-  "[TEST #3] Los productos deben tener id generado automáticamente sin repetirse."
-);
-console.log(
-  "[TEST #4] getProducts: Debe devolver los productos recién creados."
-);
+  //Se llamará el método "getProducts" nuevamente, esta vez debe aparecer el producto recién agregado
+  console.log(
+    "[TEST #3] Los productos deben tener id generado automáticamente sin repetirse."
+  );
+  console.log(
+    "[TEST #4] getProducts: Debe devolver los productos recién creados."
+  );
 
-ProductManagerTest.getProducts();
+  ProductManagerTest.getProducts();
 
-console.log(" ");
+  console.log(" ");
 
-//Se evaluará que "getProductById" devuelva error si no encuentra el producto o el producto en caso
-//de encontrarlo
-console.log("[TEST #5] getProductBy(1): Id válido");
+  //Se evaluará que "getProductById" devuelva error si no encuentra el producto o el producto en caso
+  //de encontrarlo
+  console.log("[TEST #5] getProductBy(1): Id válido");
 
-ProductManagerTest.getProductById(1);
+  ProductManagerTest.getProductById(1);
 
-console.log(" ");
-console.log("[TEST #6] getProductBy(465213): Id inválido");
+  console.log(" ");
+  console.log("[TEST #6] getProductBy(465213): Id inválido");
 
-ProductManagerTest.getProductById(465213);
+  ProductManagerTest.getProductById(465213);
 
-console.log(" ");
+  console.log(" ");
 
-// Se evaluará que "updateProduct" cambie un campo de algún producto sin eliminar el ID y que lo haya
-// actualizado
-console.log(
-  "[TEST #7] updateProduct(1, {title: 'Nuevo titulo, price: 5555}): Actualiza los campos title y price del Product.ID = 1"
-);
+  // Se evaluará que "updateProduct" cambie un campo de algún producto sin eliminar el ID y que lo haya
+  // actualizado
+  console.log(
+    "[TEST #7] updateProduct(1, {title: 'Nuevo titulo, price: 5555}): Actualiza los campos title y price del Product.ID = 1"
+  );
 
-ProductManagerTest.updateProduct(1, { title: "Nuevo titulo", price: 5555 });
+  await ProductManagerTest.updateProduct(1, {
+    title: "Nuevo titulo IX",
+    price: 1234,
+  });
 
-ProductManagerTest.getProducts();
+  ProductManagerTest.getProducts();
 
-console.log(" ");
+  console.log(" ");
 
-// Se evaluará que "deleteProduct" elimine el producto en caso de encontrarlo o arroje un error en caso
-// de no existir
-console.log("[TEST #8] deleteProduct(1): Id válido");
+  // Se evaluará que "deleteProduct" elimine el producto en caso de encontrarlo o arroje un error en caso
+  // de no existir
+  console.log("[TEST #8] deleteProduct(1): Id válido");
 
-ProductManagerTest.deleteProduct(1);
+  await ProductManagerTest.deleteProduct(1);
 
-ProductManagerTest.getProducts();
+  ProductManagerTest.getProducts();
 
-console.log(" ");
-console.log("[TEST #9] deleteProduct(465213): Id inválido");
+  console.log(" ");
+  console.log("[TEST #9] deleteProduct(465213): Id inválido");
 
-ProductManagerTest.deleteProduct(465213);
+  await ProductManagerTest.deleteProduct(465213);
 
-ProductManagerTest.getProducts();
+  ProductManagerTest.getProducts();
 
-console.log(" ");
+  console.log(" ");
+}
+
+runTests();
